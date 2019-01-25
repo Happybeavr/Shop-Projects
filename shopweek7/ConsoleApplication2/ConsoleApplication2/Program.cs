d349512156d7b49aa637e0affa19c067a2d5362a
Happybeavr <40033189+Happybeavr@users.noreply.github.com>
2019-01-25 12:39:15 -0500

    Add files via upload

---
+++ shopweek7/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! What is your name? ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Nice to meet you, " + userName + ". Now, what is your age? ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 1 || age > 99){
+                Console.WriteLine("Please enter a valid age. Enter R to restart.");
+                string invalid = Console.ReadLine();
+                if (invalid.ToUpper() == "R"){
+                    Environment.Exit(0);
+                }else
+                {
+                    Console.WriteLine("Wow aren't you original");
+                    Console.ReadLine();
+                    Environment.Exit(0);
+                }
+            }
+            if (age < 14 && age >= 1)
+            {
+                Console.WriteLine("lol u small");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+           if (age >= 14 && age <= 18)
+            {
+                Console.WriteLine("How are you liking high school so far?");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            if (age >= 19 && age <= 99)
+            {
+                Console.WriteLine("Congrats on graduating from high school!");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+        }
+    }
+}
---
+++ shopweek7/ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many hours of gaming do you do a day?");
+            int gamingHours = Convert.ToInt32(Console.ReadLine());
+
+            if (gamingHours == 18)
+            {
+                Console.WriteLine("You are a legendary gamer.");
+                Console.ReadLine();
+            }else if (gamingHours < 18)
+            {
+                int needed = 18 - gamingHours;
+                Console.WriteLine("You need " + needed + " more hours to be a true gamer.");
+                Console.ReadLine();
+            }else if (gamingHours > 18)
+            {
+                Console.WriteLine("You are...   T H E  A S C E N D E D  G A M E R");
+                Console.ReadLine();
+            }
+        }
+    }
+}
